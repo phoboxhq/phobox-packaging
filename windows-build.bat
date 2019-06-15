dbbf6bd950a466664e3a5da710f1905606a597d5
Nick <544436+Milchreis@users.noreply.github.com>
2019-06-15 11:46:27 +0200

    Update windows-build.bat

--- windows-build.bat
+++ windows-build.bat
@@ -1,1 +1,1 @@
-javapackager -deploy -native exe -outdir ./dist -srcfiles phobox.jar -outfile phobox -name phobox -title "Phobox" -appclass org.springframework.boot.loader.JarLauncher -v
+javapackager -deploy -native msi -outdir ./dist -srcfiles phobox.jar -outfile phobox -name phobox -title "Phobox" -appclass org.springframework.boot.loader.JarLauncher -v
