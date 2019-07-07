0ae799fc4ce326eabe4dcc4f23f7ced2f0453a7f
Nick <544436+Milchreis@users.noreply.github.com>
2019-07-07 22:51:07 +0200

    Create phobox-on-raspberry-pi.sh
    Script for installation phobox and java on raspbian

---
+++ phobox-on-raspberry-pi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+version="1.0.0"
+
+if ! type -p java; then
+	echo "Download java"
+	echo "-------------------------"
+	# Get and install java from bellsoft (arm build and includes javafx)
+	wget https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.deb
+
+	echo "Install java"
+	echo "-------------------------"
+	sudo apt-get install ./bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.deb 
+	sudo update-alternatives --config javac
+	sudo update-alternatives --config java
+fi
+
+if [ ! -f phobox.jar ]; then
+	# Get phobox as jar file
+	wget https://github.com/phoboxhq/phobox/releases/download/v$version/phobox-server-$version.jar -O phobox.jar
+fi
+
+java -Djavafx.platform=gtk2 -jar phobox.jar
